ad069061f7b12aaafb9e225f97c39734ba2f48db
fbc <849067797@qq.com>
2019-08-22 15:52:21 +0800

    修改 配置文件注释

--- RT1052_Pro/21-信号量/1-二值信号量-标志事件/μCOS-III/NXP/MIMXRT1050-EVK/os_cfg_app.h
+++ RT1052_Pro/21-信号量/1-二值信号量-标志事件/μCOS-III/NXP/MIMXRT1050-EVK/os_cfg_app.h
@@ -1,72 +1,32 @@
-/*
-************************************************************************************************************************
-*                                                      uC/OS-III
-*                                                 The Real-Time Kernel
-*
-*                                  (c) Copyright 2009-2017; Micrium, Inc.; Weston, FL
-*                           All rights reserved.  Protected by international copyright laws.
-*
-*                                       OS CONFIGURATION (APPLICATION SPECIFICS)
-*
-* File    : OS_CFG_APP.H
-* By      : JJL
-* Version : V3.06.02
-*
-* LICENSING TERMS:
-* ---------------
-*           uC/OS-III is provided in source form for FREE short-term evaluation, for educational use or
-*           for peaceful research.  If you plan or intend to use uC/OS-III in a commercial application/
-*           product then, you need to contact Micrium to properly license uC/OS-III for its use in your
-*           application/product.   We provide ALL the source code for your convenience and to help you
-*           experience uC/OS-III.  The fact that the source is provided does NOT mean that you can use
-*           it commercially without paying a licensing fee.
-*
-*           Knowledge of the source code may NOT be used to develop a similar product.
-*
-*           Please help us continue to provide the embedded community with the finest software available.
-*           Your honesty is greatly appreciated.
-*
-*           You can find our product's user manual, API reference, release notes and
-*           more information at doc.micrium.com.
-*           You can contact us at www.micrium.com.
-************************************************************************************************************************
-*/
-
 #ifndef OS_CFG_APP_H
 #define OS_CFG_APP_H
 
-/*
-************************************************************************************************************************
-*                                                      CONSTANTS
-************************************************************************************************************************
-*/
-                                                                /* ------------------ MISCELLANEOUS ------------------- */
-#define  OS_CFG_ISR_STK_SIZE                         256u       /* Stack size of ISR stack (number of CPU_STK elements) */
+/* ------------------ 其他配置 ------------------- */
+#define  OS_CFG_ISR_STK_SIZE                         256u       /*ISR堆栈的大小*/
 
-#define  OS_CFG_MSG_POOL_SIZE                         32u       /* Maximum number of messages                           */
+#define  OS_CFG_MSG_POOL_SIZE                         32u       /* 支持的最大消息数量*/
 
-#define  OS_CFG_TASK_STK_LIMIT_PCT_EMPTY              10u       /* Stack limit position in percentage to empty          */
+#define  OS_CFG_TASK_STK_LIMIT_PCT_EMPTY              10u      	/*检查堆栈的剩余大小（百分百形式此处是10%*/
+
+/* -------------------- 空闲任务 --------------------- */
+#define  OS_CFG_IDLE_TASK_STK_SIZE                    64u       /* 空闲任务堆栈大小*/
 
 
-                                                                /* -------------------- IDLE TASK --------------------- */
-#define  OS_CFG_IDLE_TASK_STK_SIZE                    64u       /* Stack size (number of CPU_STK elements)              */
+/* ------------------ 统计任务 ------------------ */
+#define  OS_CFG_STAT_TASK_PRIO  ((OS_PRIO)(OS_CFG_PRIO_MAX-2u)) /* 优先级*/
+#define  OS_CFG_STAT_TASK_RATE_HZ                     10u       /* 执行率（1至10 Hz）*/
+#define  OS_CFG_STAT_TASK_STK_SIZE                   128u       /* 堆栈大小（CPU_STK元素的数量）*/
 
 
-                                                                /* ------------------ STATISTIC TASK ------------------ */
-#define  OS_CFG_STAT_TASK_PRIO  ((OS_PRIO)(OS_CFG_PRIO_MAX-2u)) /* Priority                                             */
-#define  OS_CFG_STAT_TASK_RATE_HZ                     10u       /* Rate of execution (1 to 10 Hz)                       */
-#define  OS_CFG_STAT_TASK_STK_SIZE                   128u       /* Stack size (number of CPU_STK elements)              */
+/* ---------------------- 时钟节拍任务 ----------------------- */
+#define  OS_CFG_TICK_RATE_HZ                        1000u       /*系统的时钟节拍(一般为10 到 1000 Hz)*/
+#define  OS_CFG_TICK_TASK_PRIO                         1u       /*时钟节拍任务的优先级    */
+#define  OS_CFG_TICK_TASK_STK_SIZE                   128u       /*时钟节拍任务的堆栈大小 */
 
 
-                                                                /* ---------------------- TICKS ----------------------- */
-#define  OS_CFG_TICK_RATE_HZ                        1000u       /* Tick rate in Hertz (10 to 1000 Hz)                   */
-#define  OS_CFG_TICK_TASK_PRIO                         1u       /* Priority                                             */
-#define  OS_CFG_TICK_TASK_STK_SIZE                   128u       /* Stack size (number of CPU_STK elements)              */
-
-
-                                                                /* --------------------- TIMERS ----------------------- */
-#define  OS_CFG_TMR_TASK_PRIO   ((OS_PRIO)(OS_CFG_PRIO_MAX-3u)) /* Priority of 'Timer Task'                             */
-#define  OS_CFG_TMR_TASK_RATE_HZ                       1u       /* Rate for timers (10 Hz Typ.)                         */
-#define  OS_CFG_TMR_TASK_STK_SIZE                    128u       /* Stack size (number of CPU_STK elements)              */
+/* --------------------- 定时器任务 ----------------------- */
+#define  OS_CFG_TMR_TASK_PRIO   ((OS_PRIO)(OS_CFG_PRIO_MAX-3u)) /*“计时器任务”的优先级*/
+#define  OS_CFG_TMR_TASK_RATE_HZ                       1u       /*定时器的频率*/
+#define  OS_CFG_TMR_TASK_STK_SIZE                    128u       /*定时器任务的堆栈大小*/
 
 #endif
--- RT1052_Pro/22-互斥量/1-优先级翻转/μCOS-III/NXP/MIMXRT1050-EVK/os_cfg.h
+++ RT1052_Pro/22-互斥量/1-优先级翻转/μCOS-III/NXP/MIMXRT1050-EVK/os_cfg.h
@@ -1,137 +1,91 @@
-/*
-************************************************************************************************************************
-*                                                      uC/OS-III
-*                                                 The Real-Time Kernel
-*
-*                                  (c) Copyright 2009-2017; Micrium, Inc.; Weston, FL
-*                           All rights reserved.  Protected by international copyright laws.
-*
-*                                                  CONFIGURATION FILE
-*
-* File    : OS_CFG.H
-* By      : JJL
-* Version : V3.06.02
-*
-* LICENSING TERMS:
-* ---------------
-*           uC/OS-III is provided in source form for FREE short-term evaluation, for educational use or
-*           for peaceful research.  If you plan or intend to use uC/OS-III in a commercial application/
-*           product then, you need to contact Micrium to properly license uC/OS-III for its use in your
-*           application/product.   We provide ALL the source code for your convenience and to help you
-*           experience uC/OS-III.  The fact that the source is provided does NOT mean that you can use
-*           it commercially without paying a licensing fee.
-*
-*           Knowledge of the source code may NOT be used to develop a similar product.
-*
-*           Please help us continue to provide the embedded community with the finest software available.
-*           Your honesty is greatly appreciated.
-*
-*           You can find our product's user manual, API reference, release notes and
-*           more information at doc.micrium.com.
-*           You can contact us at www.micrium.com.
-************************************************************************************************************************
-*/
-
 #ifndef OS_CFG_H
 #define OS_CFG_H
 
-                                                                /* --------------------------- MISCELLANEOUS --------------------------- */
-#define OS_CFG_APP_HOOKS_EN             DEF_ENABLED               /* Enable (DEF_ENABLED) application specific hooks                       */
-#define OS_CFG_ARG_CHK_EN               DEF_ENABLED               /* Enable (DEF_ENABLED) argument checking                                */
-#define OS_CFG_CALLED_FROM_ISR_CHK_EN   DEF_ENABLED               /* Enable (DEF_ENABLED) check for called from ISR                        */
-#define OS_CFG_DBG_EN                   DEF_ENABLED               /* Enable (DEF_ENABLED) debug code/variables                             */
-#define OS_CFG_DYN_TICK_EN              DEF_DISABLED              /* Enable (DEF_ENABLED) the Dynamic Tick                                 */
-#define OS_CFG_INVALID_OS_CALLS_CHK_EN  DEF_ENABLED               /* Enable (DEF_ENABLED) checks for invalid kernel calls                  */
-#define OS_CFG_OBJ_TYPE_CHK_EN          DEF_ENABLED               /* Enable (DEF_ENABLED) object type checking                             */
-#define OS_CFG_TS_EN                    DEF_ENABLED              /* Enable (DEF_ENABLED) time stamping                                    */
+/* -------------- 其他配置 --------------- */                                               
+#define OS_CFG_APP_HOOKS_EN             DEF_ENABLED    /* 是否使用子函数*/
+#define OS_CFG_ARG_CHK_EN               DEF_ENABLED    /* 是否使用参数检查*/
+#define OS_CFG_CALLED_FROM_ISR_CHK_EN   DEF_ENABLED    /* 是否使用中断调用检查*/
+#define OS_CFG_DBG_EN                   DEF_ENABLED    /* 是否使用debug*/
+#define OS_CFG_DYN_TICK_EN              DEF_DISABLED   /* 是否使用动态勾选*/
+#define OS_CFG_INVALID_OS_CALLS_CHK_EN  DEF_ENABLED    /* 是否使用检查无效的内核调用 */
+#define OS_CFG_OBJ_TYPE_CHK_EN          DEF_ENABLED    /* 是否使用对象类型检查*/
+#define OS_CFG_TS_EN                    DEF_ENABLED    /* 是否使用时间戳 */
 
-#define OS_CFG_PRIO_MAX                 64u                     /* Defines the maximum number of task priorities (see OS_PRIO data type) */
+#define OS_CFG_PRIO_MAX                 64u            /* 定义任务优先级的最大数量（请参阅OS_PRIO数据类型*/
 
-#define OS_CFG_SCHED_LOCK_TIME_MEAS_EN  DEF_DISABLED            /* Include (DEF_ENABLED) code to measure scheduler lock time             */
-#define OS_CFG_SCHED_ROUND_ROBIN_EN     DEF_ENABLED             /* Include (DEF_ENABLED) code for Round-Robin scheduling                 */
+#define OS_CFG_SCHED_LOCK_TIME_MEAS_EN  DEF_DISABLED   /* 是否包含（DEF_ENABLED）代码以测量调度程序锁定时间*/
+#define OS_CFG_SCHED_ROUND_ROBIN_EN     DEF_ENABLED    /* 是否包含（DEF_ENABLED）循环调度的代码*/
 
-#define OS_CFG_STK_SIZE_MIN             64u                     /* Minimum allowable task stack size                                     */
-
-
-                                                                /* --------------------------- EVENT FLAGS ----------------------------- */
-#define OS_CFG_FLAG_EN                  DEF_ENABLED             /* Enable (DEF_ENABLED) code generation for EVENT FLAGS                  */
-#define OS_CFG_FLAG_DEL_EN              DEF_ENABLED             /*     Include (DEF_ENABLED) code for OSFlagDel()                        */
-#define OS_CFG_FLAG_MODE_CLR_EN         DEF_ENABLED             /*     Include (DEF_ENABLED) code for Wait on Clear EVENT FLAGS          */
-#define OS_CFG_FLAG_PEND_ABORT_EN       DEF_ENABLED             /*     Include (DEF_ENABLED) code for OSFlagPendAbort()                  */
+#define OS_CFG_STK_SIZE_MIN             64u            /*最小允许任务堆栈大小*/
 
 
-                                                                /* ------------------------ MEMORY MANAGEMENT -------------------------  */
-#define OS_CFG_MEM_EN                   DEF_ENABLED             /* Enable (DEF_ENABLED) code generation for the MEMORY MANAGER           */
+/* -------------- 事件标志位------------- */                                                  
+#define OS_CFG_FLAG_EN                  DEF_ENABLED    /*是否使用事件标志位*/
+#define OS_CFG_FLAG_DEL_EN              DEF_ENABLED    /*是否包含OSFlagDel()的代码 */
+#define OS_CFG_FLAG_MODE_CLR_EN         DEF_ENABLED    /*是否包含清除事件标志位的代码 */
+#define OS_CFG_FLAG_PEND_ABORT_EN       DEF_ENABLED    /*是否包含OSFlagPendAbort()的代码*/
 
 
-                                                                /* ------------------- MUTUAL EXCLUSION SEMAPHORES --------------------  */
-#define OS_CFG_MUTEX_EN                 DEF_ENABLED             /* Enable (DEF_ENABLED) code generation for MUTEX                        */
-#define OS_CFG_MUTEX_DEL_EN             DEF_ENABLED             /*     Include (DEF_ENABLED) code for OSMutexDel()                       */
-#define OS_CFG_MUTEX_PEND_ABORT_EN      DEF_ENABLED             /*     Include (DEF_ENABLED) code for OSMutexPendAbort()                 */
+ /* --------------- 内存管理 ----------- */                                                      
+#define OS_CFG_MEM_EN                   DEF_ENABLED     /* 是否使用内存管理 */
 
 
-                                                                /* -------------------------- MESSAGE QUEUES --------------------------  */
-#define OS_CFG_Q_EN                     DEF_ENABLED             /* Enable (DEF_ENABLED) code generation for QUEUES                       */
-#define OS_CFG_Q_DEL_EN                 DEF_ENABLED             /*     Include (DEF_ENABLED) code for OSQDel()                           */
-#define OS_CFG_Q_FLUSH_EN               DEF_ENABLED             /*     Include (DEF_ENABLED) code for OSQFlush()                         */
-#define OS_CFG_Q_PEND_ABORT_EN          DEF_ENABLED             /*     Include (DEF_ENABLED) code for OSQPendAbort()                     */
+ /* --------------- 互斥量 ------------- */                                                   
+#define OS_CFG_MUTEX_EN                 DEF_ENABLED    /*是否使用互斥量 */
+#define OS_CFG_MUTEX_DEL_EN             DEF_ENABLED    /*是否包含OSMutexDel()的代码*/
+#define OS_CFG_MUTEX_PEND_ABORT_EN      DEF_ENABLED    /*是否包含OSMutexPendAbort()的代码 */
 
 
-                                                                /* ---------------------------- SEMAPHORES ----------------------------- */
-#define OS_CFG_SEM_EN                   DEF_ENABLED             /* Enable (DEF_ENABLED) code generation for SEMAPHORES                   */
-#define OS_CFG_SEM_DEL_EN               DEF_ENABLED             /*     Include (DEF_ENABLED) code for OSSemDel()                         */
-#define OS_CFG_SEM_PEND_ABORT_EN        DEF_ENABLED             /*     Include (DEF_ENABLED) code for OSSemPendAbort()                   */
-#define OS_CFG_SEM_SET_EN               DEF_ENABLED             /*     Include (DEF_ENABLED) code for OSSemSet()                         */
+ /* ---------- 消息队列---------------- */                                                      
+#define OS_CFG_Q_EN                     DEF_ENABLED    /*是否使用消息队列       */                  
+#define OS_CFG_Q_DEL_EN                 DEF_ENABLED    /*是否包含OSQDel()的代码 */                  
+#define OS_CFG_Q_FLUSH_EN               DEF_ENABLED    /*是否包含OSQFlush()的代码 */                
+#define OS_CFG_Q_PEND_ABORT_EN          DEF_ENABLED    /*是否包含OSQPendAbort()的代码 */            
 
 
-                                                                /* -------------------------- TASK MANAGEMENT -------------------------- */
-#define OS_CFG_STAT_TASK_EN             DEF_ENABLED             /* Enable (DEF_ENABLED) the statistics task                              */
-#define OS_CFG_STAT_TASK_STK_CHK_EN     DEF_ENABLED             /*     Check task stacks (DEF_ENABLED) from the statistic task           */
-
-#define OS_CFG_TASK_CHANGE_PRIO_EN      DEF_ENABLED             /* Include (DEF_ENABLED) code for OSTaskChangePrio()                     */
-#define OS_CFG_TASK_DEL_EN              DEF_ENABLED             /* Include (DEF_ENABLED) code for OSTaskDel()                            */
-#define OS_CFG_TASK_IDLE_EN             DEF_ENABLED             /* Include (DEF_ENABLED) the idle task                                   */
-#define OS_CFG_TASK_PROFILE_EN          DEF_ENABLED             /* Include (DEF_ENABLED) variables in OS_TCB for profiling               */
-#define OS_CFG_TASK_Q_EN                DEF_ENABLED             /* Include (DEF_ENABLED) code for OSTaskQXXXX()                          */
-#define OS_CFG_TASK_Q_PEND_ABORT_EN     DEF_ENABLED             /* Include (DEF_ENABLED) code for OSTaskQPendAbort()                     */
-#define OS_CFG_TASK_REG_TBL_SIZE        1u                      /* Number of task specific registers                                     */
-#define OS_CFG_TASK_STK_REDZONE_EN      DEF_DISABLED            /* Enable (DEF_ENABLED) stack redzone                                    */
-#define OS_CFG_TASK_STK_REDZONE_DEPTH   8u                      /* Depth of the stack redzone                                        */
-#define OS_CFG_TASK_SEM_PEND_ABORT_EN   DEF_ENABLED             /* Include (DEF_ENABLED) code for OSTaskSemPendAbort()                   */
-#define OS_CFG_TASK_SUSPEND_EN          DEF_ENABLED             /* Include (DEF_ENABLED) code for OSTaskSuspend() and OSTaskResume()     */
-#define OS_CFG_TASK_TICK_EN             DEF_ENABLED             /* Include (DEF_ENABLED) the kernel tick task                            */
+ /* --------------- 信号量 ------------ */                                                     
+#define OS_CFG_SEM_EN                   DEF_ENABLED    /*是否使用信号量  */        					
+#define OS_CFG_SEM_DEL_EN               DEF_ENABLED    /*是否包含OSSemDel()的代码 */       
+#define OS_CFG_SEM_PEND_ABORT_EN        DEF_ENABLED    /*是否包含OSSemPendAbort()的代码*/  
+#define OS_CFG_SEM_SET_EN               DEF_ENABLED    /*是否包含OSSemSet()的代码  */      
 
 
-                                                                /* ------------------ TASK LOCAL STORAGE MANAGEMENT -------------------  */
-#define OS_CFG_TLS_TBL_SIZE             0u                      /* Include (DEF_ENABLED) code for Task Local Storage (TLS) registers     */
+ /* ----------- 任务管理 -------------- */                                                     
+#define OS_CFG_STAT_TASK_EN             DEF_ENABLED    /*是否使用任务统计功能 */
+#define OS_CFG_STAT_TASK_STK_CHK_EN     DEF_ENABLED    /*从统计任务中检查任务堆栈 */
+
+#define OS_CFG_TASK_CHANGE_PRIO_EN      DEF_ENABLED    /*是否包含OSTaskChangePrio()代码*/
+#define OS_CFG_TASK_DEL_EN              DEF_ENABLED    /*是否包含OSTaskDel()代码*/
+#define OS_CFG_TASK_IDLE_EN             DEF_ENABLED    /*是否使用空闲任务*/
+#define OS_CFG_TASK_PROFILE_EN          DEF_ENABLED    /*是否使用OS_TCB中的变量用来分析*/
+#define OS_CFG_TASK_Q_EN                DEF_ENABLED    /*是否包含OSTaskQXXXX()代码*/
+#define OS_CFG_TASK_Q_PEND_ABORT_EN     DEF_ENABLED    /*是否包含OSTaskQPendAbort()代码*/
+#define OS_CFG_TASK_REG_TBL_SIZE        1u             /*任务特定寄存器的数量*/
+#define OS_CFG_TASK_STK_REDZONE_EN      DEF_DISABLED   /*是否使用堆栈 redzone */
+#define OS_CFG_TASK_STK_REDZONE_DEPTH   8u             /*堆栈redzone的深度*/
+#define OS_CFG_TASK_SEM_PEND_ABORT_EN   DEF_ENABLED    /*是否包含OSTaskSemPendAbort()代码*/
+#define OS_CFG_TASK_SUSPEND_EN          DEF_ENABLED    /*是否包含OSTaskSuspend()和OSTaskResume()代码 */
+#define OS_CFG_TASK_TICK_EN             DEF_ENABLED    /*是否包含（DEF_ENABLED）内核tick任务*/
 
 
-                                                                /* ------------------------- TIME MANAGEMENT --------------------------  */
-#define OS_CFG_TIME_DLY_HMSM_EN         DEF_ENABLED             /* Include (DEF_ENABLED) code for OSTimeDlyHMSM()                        */
-#define OS_CFG_TIME_DLY_RESUME_EN       DEF_ENABLED             /* Include (DEF_ENABLED) code for OSTimeDlyResume()                      */
+/* ------- 任务本地存储管理 ------- */                                                   
+#define OS_CFG_TLS_TBL_SIZE             0u             /* 是否包含任务本地存储（TLS）寄存器的代码（DEF_ENABLED）*/
 
 
-                                                                /* ------------------------- TIMER MANAGEMENT -------------------------- */
-#define OS_CFG_TMR_EN                   DEF_ENABLED             /* Enable (DEF_ENABLED) code generation for TIMERS                       */
-#define OS_CFG_TMR_DEL_EN               DEF_ENABLED             /* Enable (DEF_ENABLED) code generation for OSTmrDel()                   */
+ /* -------- 时间管理 ------------- */                                                     
+#define OS_CFG_TIME_DLY_HMSM_EN         DEF_ENABLED    /*是否包含OSTimeDlyHMSM()的代码*/
+#define OS_CFG_TIME_DLY_RESUME_EN       DEF_ENABLED    /*是否包含OSTimeDlyResume()的代码*/
 
 
-                                                                /* ------------------------- TRACE RECORDER ---------------------------- */
-#define OS_CFG_TRACE_EN                 DEF_DISABLED            /* Enable (DEF_ENABLED) uC/OS-III Trace instrumentation                  */
-#define OS_CFG_TRACE_API_ENTER_EN       DEF_DISABLED            /* Enable (DEF_ENABLED) uC/OS-III Trace API enter instrumentation        */
-#define OS_CFG_TRACE_API_EXIT_EN        DEF_DISABLED            /* Enable (DEF_ENABLED) uC/OS-III Trace API exit  instrumentation        */
+ /* ---------- 定时器管理 -------- */                                                     
+#define OS_CFG_TMR_EN                   DEF_ENABLED    /*是否使用定时器 */
+#define OS_CFG_TMR_DEL_EN               DEF_ENABLED    /*是否支持OSTmrDel()*/
 
 
-/*
-*********************************************************************************************************
-*********************************************************************************************************
-*                               WARNING - DEPRECATION NOTICE - WARNING
-* The following options are part of a deprecated feature and will be removed in a future release.
-*********************************************************************************************************
-*********************************************************************************************************
-*/
-                                                           /* ----------------------------- MONITORS ------------------------------ */
-#define OS_CFG_MON_EN                   DEF_DISABLED       /* Enable (DEF_ENABLED) code generation for MONITORS                     */
-#define OS_CFG_MON_DEL_EN               DEF_DISABLED       /*     Include (DEF_ENABLED) code for OSMonDel()                         */
+/*-----------跟踪录音机-----------*/                       
+#define OS_CFG_TRACE_EN                 DEF_DISABLED   /*是否使用 uC/OS-III 跟踪仪器*/
+#define OS_CFG_TRACE_API_ENTER_EN       DEF_DISABLED   /*是否使用 uC/OS-III Trace API进入检测*/
+#define OS_CFG_TRACE_API_EXIT_EN        DEF_DISABLED   /*是否使用 uC/OS-III Trace API退出检测*/
+
 
 #endif
